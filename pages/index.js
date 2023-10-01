f1b65cbb315ed19fb6fd9f4f6b508406be876454
Vishwanthkarka <vishwanathreddykarka@gmail.com>
2023-10-01 23:50:08 +0530

    worked on the home page

--- pages/index.js
+++ pages/index.js
@@ -13,8 +13,8 @@ export default function Home() {
     <Header/>
     <div className='py-4 h-[90vh] flex flex-wrap  justify-center'>
 <div className='px-[5rem] mt-[1rem] lg:mt-[9rem] min-w-[100vw] lg:min-w-[300px] flex justify-top flex-col items-center'>
-  <h5 className='text-white text-[2rem] md:text-[4rem] font-bold '>Your child<br></br> is a <span className='underline decoration-wavy underline-offset-8 text-primarycolor decoration-4 p-2 '>Hero !</span> </h5>
-  <p className='text-[#717377] md:text-[1.5rem] text-[1rem] mb-8'>Join them in our XYZ school</p>
+  <h5 className='text-white text-[2rem] md:text-[4rem] font-bold '>Welcome Every One </h5>
+  <p className='text-[#717377] md:text-[1.5rem] text-[1rem] mb-8'>Let's Pay games</p>
   <Link href="/login" className='bg-white ml-4  no-underline text-secoundblack  px-[2rem] py-[1rem] rounded-[30px] font-bold'>Start Now </Link>
 </div>
 <div className='lg:w-[500px] w-[100vw] text-center order-[-1]  lg:order-1 flex justify-center'>
